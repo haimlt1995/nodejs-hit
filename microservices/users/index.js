3214dc95d8c061bbad0986acce2d20f980aa2a62
Tamir Shevchenko Kashy <87029988+TamirKashy@users.noreply.github.com>
2026-08-19 14:40:11 +0300

    Update index.js

--- microservices/users/index.js
+++ microservices/users/index.js
@@ -4,6 +4,6 @@ import { userRouter } from './user.routes.js';
 
 // the service that handles everything about users
 const SERVICE_NAME = 'users';
-const DEFAULT_PORT = 3002;
+const DEFAULT_PORT = 3000;
 
 await startService(SERVICE_NAME, DEFAULT_PORT, [addRouter, userRouter]);
